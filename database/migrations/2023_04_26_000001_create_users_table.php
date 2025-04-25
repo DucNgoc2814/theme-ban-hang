9887e54b9b0363db4579998619664ca959be41f1
ngoc <phungducngoc28@gmail.com>
2025-04-26 01:11:07 +0700

    migration

--- database/migrations/2023_04_26_000001_create_users_table.php
+++ database/migrations/2023_04_26_000001_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->constrained('user_roles');
             $table->string('name');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
